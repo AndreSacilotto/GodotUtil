890b93a2cf0def3c20606e2882cf314d878c7543
Andre Sacilotto <66838864+AndreSacilotto@users.noreply.github.com>
2022-10-21 08:27:59 -0300

    Calculator Expressions

---
+++ Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Util
+{
+	public static class Calculator<T> where T : unmanaged
+	{
+		public delegate bool MathFuncCmp(T a, T b);
+		public delegate T MathFuncPlural(T a, T b);
+		public delegate T MathFuncSingle(T a);
+
+		public static MathFuncPlural Add { get; } = CreateExpression<MathFuncPlural>(Expression.Add);
+		public static MathFuncPlural Subtract { get; } = CreateExpression<MathFuncPlural>(Expression.Subtract);
+		public static MathFuncPlural Multiply { get; } = CreateExpression<MathFuncPlural>(Expression.Multiply);
+		public static MathFuncPlural Divide { get; } = CreateExpression<MathFuncPlural>(Expression.Divide);
+		
+		public static MathFuncSingle Negate { get; } = CreateExpression(Expression.Negate);
+		public static MathFuncSingle Increment { get; } = CreateExpression(Expression.Increment);
+		public static MathFuncSingle Decrement { get; } = CreateExpression(Expression.Decrement);
+
+		public static MathFuncCmp Greater { get; } = CreateExpression<MathFuncCmp>(Expression.GreaterThan);
+		public static MathFuncCmp GreaterEqual { get; } = CreateExpression<MathFuncCmp>(Expression.GreaterThanOrEqual);
+		public static MathFuncCmp Less { get; } = CreateExpression<MathFuncCmp>(Expression.LessThan);
+		public static MathFuncCmp LessEqual { get; } = CreateExpression<MathFuncCmp>(Expression.LessThanOrEqual);
+		public static MathFuncCmp Equal { get; } = CreateExpression<MathFuncCmp>(Expression.Equal);
+		public static MathFuncCmp NotEqual { get; } = CreateExpression<MathFuncCmp>(Expression.NotEqual);
+
+		private static MF CreateExpression<MF>(Func<Expression, Expression, Expression> exprFunc)
+		{
+			var paramA = Expression.Parameter(typeof(T));
+			var paramB = Expression.Parameter(typeof(T));
+			var body = exprFunc(paramA, paramB);
+			return Expression.Lambda<MF>(body, paramA, paramB).Compile();
+		}
+
+		private static MathFuncSingle CreateExpression(Func<Expression, Expression> exprFunc)
+		{
+			var paramA = Expression.Parameter(typeof(T));
+			var body = exprFunc(paramA);
+			return Expression.Lambda<MathFuncSingle>(body, paramA).Compile();
+		}
+	}
+}
--- UtilReflection.cs
+++ UtilReflection.cs
@@ -1,57 +1,79 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Collections.Generic;
 using System.Reflection;
 
 namespace Util
 {
-    public static class UtilReflection
-    {
-        public static readonly List<Assembly> assemblies;
-        public static IReadOnlyList<Assembly> Assemblies => assemblies;
+	public static class UtilReflection
+	{
+		public static readonly List<Assembly> assemblies;
+		public static IReadOnlyList<Assembly> Assemblies => assemblies;
 
-        static UtilReflection()
-        {
-            assemblies = new List<Assembly>(AppDomain.CurrentDomain.GetAssemblies());
-            AppDomain.CurrentDomain.AssemblyLoad += (sender, args) => assemblies.Add(args.LoadedAssembly);
-        }
+		static UtilReflection()
+		{
+			assemblies = new List<Assembly>(AppDomain.CurrentDomain.GetAssemblies());
+			AppDomain.CurrentDomain.AssemblyLoad += (sender, args) => assemblies.Add(args.LoadedAssembly);
+		}
 
-        public static Type GetType(string typeName)
-        {
-            Type type = Type.GetType(typeName);
-            if (type == null)
-            {
-                for (int i = assemblies.Count - 1; i >= 0; i--)
-                {
-                    type = assemblies[i].GetType(typeName);
-                    if (type != null)
-                        return type;
-                }
-            }
-            return type;
-        }
+		public static Type GetType(string typeName)
+		{
+			Type type = Type.GetType(typeName);
+			if (type == null)
+			{
+				for (int i = assemblies.Count - 1; i >= 0; i--)
+				{
+					type = assemblies[i].GetType(typeName);
+					if (type != null)
+						return type;
+				}
+			}
+			return type;
+		}
 
-        public static Assembly[] GetAllAssemblies()
-        {
-            var assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
-            var assemblyRefs = new Assembly[assemblyNames.Length];
-            for (int i = 0; i < assemblyNames.Length; i++)
-                assemblyRefs[i] = Assembly.Load(assemblyNames[i]);
-            return assemblyRefs;
-        }
+		public static Assembly[] GetAllAssemblies()
+		{
+			var assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+			var assemblyRefs = new Assembly[assemblyNames.Length];
+			for (int i = 0; i < assemblyNames.Length; i++)
+				assemblyRefs[i] = Assembly.Load(assemblyNames[i]);
+			return assemblyRefs;
+		}
 
-        public static T GetFieldValue<T>(object obj, string name)
-        {
-            var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-            var field = obj.GetType().GetField(name, bindingFlags);
-            return (T)field?.GetValue(obj);
-        }
+		public static T GetFieldValue<T>(object obj, string name)
+		{
+			var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+			var field = obj.GetType().GetField(name, bindingFlags);
+			return (T)field?.GetValue(obj);
+		}
 
-        public static T GetPropValue<T>(object obj, string name)
-        {
-            var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-            var prop = obj.GetType().GetProperty(name, bindingFlags);
-            return (T)prop?.GetValue(obj);
-        }
+		public static T GetPropValue<T>(object obj, string name)
+		{
+			var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+			var prop = obj.GetType().GetProperty(name, bindingFlags);
+			return (T)prop?.GetValue(obj);
+		}
 
-    }
+		public delegate object ConstructorFunc(params object[] args);
+		public static ConstructorFunc CreateConstructorFunc(Type constructorClassType, params Type[] constructorParams)
+		{
+			var constructorInfo = constructorClassType.GetConstructor(constructorParams);
+
+			// To feed the constructor with the right parameters, we need to generate an array 
+			// of parameters that will be read from the initialize object array argument.
+			var len = constructorParams.Length;
+			var constructorArgs = new UnaryExpression[len];
+
+			// convert the object[index] to the right constructor parameter type.
+			var paramExpr = Expression.Parameter(typeof(object[]));
+			for (int i = 0; i < len; i++)
+				constructorArgs[i] = Expression.Convert(Expression.ArrayAccess(paramExpr, Expression.Constant(i)), constructorParams[i]);
+
+			var body = Expression.New(constructorInfo, constructorArgs);
+			var constructor = Expression.Lambda<ConstructorFunc>(body, paramExpr);
+			return constructor.Compile();
+		}
+
+	}
 }
